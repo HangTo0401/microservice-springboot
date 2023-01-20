fdbce42ea59ff20ed0ccd8a812d86edcd09a5fc0
HangTo <tominhhang1996@gmail.com>
2023-01-20 20:12:15 +0700

    Implement Discovery Server

--- orders-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
+++ orders-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
@@ -8,7 +8,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
     }
+
+//    @Bean
+//    public WebClient.Builder webClientBuilder() {
+//        return WebClient.builder();
+//    }
 }
