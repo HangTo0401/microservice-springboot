bfbd594846ec1d3a2f8c8be316f325ba2e4c491f
HangTo <tominhhang1996@gmail.com>
2023-01-26 23:06:25 +0700

    Remove @SneakyThrows for Inventory service

--- inventories-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
+++ inventories-service/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
@@ -23,11 +23,7 @@ public class InventoryController {
     // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    @SneakyThrows
     public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
-        log.info("Wait started!");
-        Thread.sleep(10000);
-        log.info("Wait ended!");
         return inventoryService.isInStock(skuCode);
     }
 }
